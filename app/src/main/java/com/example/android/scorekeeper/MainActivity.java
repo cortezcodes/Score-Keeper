c9a0c43882a513af4233f8c2d136ab9c3d642583
CortezCodes <cortez.mccrary.codes@gmail.com>
2019-09-10 08:57:00 +0800

    Score Keeper Final
    Completed app as per Udacity Requirements

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -4,27 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int teamRedScore = 0;
     int teamBlueScore = 0;
+    Button redFieldGoalBtn;
+    Button redTouchdownBtn;
+    Button redExtraPointBtn;
+    Button redWentForTwoBtn;
+    Button redTouchBackBtn;
+    Button blueFieldGoalBtn;
+    Button blueTouchDownBtn;
+    Button bluExtraPointBtn;
+    Button blueWentForTwoBtn;
+    Button blueTouchBackBtn;
+    Button missedExtraBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        redFieldGoalBtn = findViewById(R.id.team_red_field_goal_button);
+        redTouchdownBtn = findViewById(R.id.team_red_touchdown_button);
+        redExtraPointBtn = findViewById(R.id.team_red_extra_point_button);
+        redWentForTwoBtn = findViewById(R.id.team_red_went_for_two_button);
+        redTouchBackBtn = findViewById(R.id.team_red_touch_back_button);
+        blueFieldGoalBtn = findViewById(R.id.team_blue_field_goal_button);
+        blueTouchDownBtn = findViewById(R.id.team_blue_touchdown_button);
+        bluExtraPointBtn = findViewById(R.id.team_blue_extra_point_button);
+        blueWentForTwoBtn = findViewById(R.id.team_blue_went_for_two_button);
+        blueTouchBackBtn = findViewById(R.id.team_blue_touch_back_button);
+        missedExtraBtn = findViewById(R.id.missed_extra_point);
+        beforeTouchdown();
     }
 
     /**
-     * Resets all variables and displays back to zero
+     * Resets all variables, displays, and button logic.
      */
     public void updateAll(View view){
         teamRedScore = 0;
         teamBlueScore = 0;
         updateTeamRedScore(0);
         updateTeamBlueScore(0);
+        beforeTouchdown();
     }
 
     /**
@@ -52,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
     public void touchdownRed(View view){
         scoreToast(1, "Red team");
         updateTeamRedScore(6);
+        afterTouchdown(true);
     }
 
     /**
@@ -61,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
     public void touchdownBlue(View view){
         scoreToast(1, "Blue team");
         updateTeamBlueScore(6);
+        afterTouchdown(false);
     }
 
     /**
@@ -70,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
     public void extraPointRed(View view){
         scoreToast(2, "Red team");
         updateTeamRedScore(1);
+        beforeTouchdown();
     }
 
     /**
@@ -79,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
     public void exraPointBlue(View view){
         scoreToast(2, "Blue team");
         updateTeamBlueScore(1);
+        beforeTouchdown();
     }
 
     /**
@@ -88,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
     public void wentForTwoRed(View view){
         scoreToast(3, "Red team");
         updateTeamRedScore(2);
+        beforeTouchdown();
     }
 
     /**
@@ -97,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
     public void wentForTwoBlue(View view){
         scoreToast(3, "Blue team");
         updateTeamBlueScore(2);
+        beforeTouchdown();
     }
 
     /**
@@ -115,6 +146,11 @@ public class MainActivity extends AppCompatActivity {
     public void touchBackBlue(View view){
         scoreToast(4, "Blue team");
         updateTeamBlueScore(2);
+    }
+
+    public void missedExtraPoint(View view){
+        scoreToast(5, "");
+        beforeTouchdown();
     }
 
 
@@ -141,6 +177,8 @@ public class MainActivity extends AppCompatActivity {
             case 4:
                 announcement = teamName + " sacks the QB in the endzone!";
                 break;
+            case 5:
+                announcement = "The kick goes wide.";
         }
 
         Toast toast = Toast.makeText(this, announcement, Toast.LENGTH_SHORT);
@@ -154,9 +192,9 @@ public class MainActivity extends AppCompatActivity {
      * @param points - int variable to be added to original teamRedScore value.
      */
     private void updateTeamRedScore(int points){
-        String score = "";
+        String score;
         teamRedScore = teamRedScore + points;
-        TextView teamRedScoreboard = (TextView) findViewById(R.id.team_red_score_textview);
+        TextView teamRedScoreboard = findViewById(R.id.team_red_score_textview);
         if (teamRedScore < 10){
             score = "0" + teamRedScore;
         }
@@ -171,9 +209,9 @@ public class MainActivity extends AppCompatActivity {
      * @param points - int variable to be added to original teamRedScore value.
      */
     private void updateTeamBlueScore(int points){
-        String score = "";
+        String score;
         teamBlueScore = teamBlueScore + points;
-        TextView teamBlueScoreboard = (TextView) findViewById(R.id.team_blue_score_textview);
+        TextView teamBlueScoreboard = findViewById(R.id.team_blue_score_textview);
         if (teamBlueScore < 10){
             score = "0" + teamBlueScore;
         }
@@ -181,6 +219,58 @@ public class MainActivity extends AppCompatActivity {
             score = Integer.toString(teamBlueScore);
         }
         teamBlueScoreboard.setText(score);
+    }
+
+    /**
+     * Sets state of all buttons displayed before a touchdown is scored. Disables the extra point,
+     * the went for two, and the missed extra point buttons. Also changes the buttons background to gray.
+     */
+    private void beforeTouchdown(){
+        missedExtraBtn.setEnabled(false);
+        redExtraPointBtn.setEnabled(false);
+        redWentForTwoBtn.setEnabled(false);
+        redFieldGoalBtn.setEnabled(true);
+        redTouchdownBtn.setEnabled(true);
+        redTouchBackBtn.setEnabled(true);
+        blueFieldGoalBtn.setEnabled(true);
+        blueTouchDownBtn.setEnabled(true);
+        bluExtraPointBtn.setEnabled(false);
+        blueWentForTwoBtn.setEnabled(false);
+        blueTouchBackBtn.setEnabled(true);
+    }
+
+    /**
+     * Button logic after a touchdown is scored. All buttons disabled except for the scoring teams
+     * extra point, went for two, and missed extra buttons.
+     */
+    private void afterTouchdown(boolean isRedTouchdown){
+
+        if (isRedTouchdown){
+            missedExtraBtn.setEnabled(true);
+            redExtraPointBtn.setEnabled(true);
+            redWentForTwoBtn.setEnabled(true);
+            redFieldGoalBtn.setEnabled(false);
+            redTouchdownBtn.setEnabled(false);
+            redTouchBackBtn.setEnabled(false);
+            blueFieldGoalBtn.setEnabled(false);
+            blueTouchDownBtn.setEnabled(false);
+            bluExtraPointBtn.setEnabled(false);
+            blueWentForTwoBtn.setEnabled(false);
+            blueTouchBackBtn.setEnabled(false);
+        }
+        else{
+            missedExtraBtn.setEnabled(true);
+            redExtraPointBtn.setEnabled(false);
+            redWentForTwoBtn.setEnabled(false);
+            redFieldGoalBtn.setEnabled(false);
+            redTouchdownBtn.setEnabled(false);
+            redTouchBackBtn.setEnabled(false);
+            blueFieldGoalBtn.setEnabled(false);
+            blueTouchDownBtn.setEnabled(false);
+            bluExtraPointBtn.setEnabled(true);
+            blueWentForTwoBtn.setEnabled(true);
+            blueTouchBackBtn.setEnabled(false);
+        }
     }
 
 }
